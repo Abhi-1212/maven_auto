9969fa381488e7d3b5025798dc63310de199358e
abhishek <abhishek@DESKTOP-OJIJEQU>
2023-07-07 13:06:40 +0530

    adding maven project

---
+++ src/test/java/automationsquad/project/Testfrom.java
@@ -0,0 +1,16 @@
+package automationsquad.project;
+
+import org.testng.annotations.Test;
+
+public class Testfrom {
+@Test
+public void fr() {
+	System.out.println("hello ew");
+}
+
+
+@Test
+public void fre() {
+	System.out.println("hello hgh");
+}
+}
---
+++ src/test/java/automationsquad/project/Testhome.java
@@ -0,0 +1,14 @@
+package automationsquad.project;
+
+import org.testng.annotations.Test;
+
+public class Testhome {
+  @Test
+  public void gg() {
+	  System.out.println("hello abhi1");
+  }
+  @Test
+  public void tr() {
+	  System.out.println("hello abhi2");
+  }
+}
